b33eaf7751b4144cbe06c5889e7b98a9e8db61d5
Ekaterina1971 <ivanova71dom@yandex.ru>
2024-10-31 19:21:27 +0300

    начальный коммит

---
+++ src/main/java/org/skypro/list/ListOfEmployeApplication.java
@@ -0,0 +1,13 @@
+package org.skypro.list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListOfEmployeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListOfEmployeApplication.class, args);
+	}
+
+}

4128c75c303539f0ce8323816dbde842aef51d26
Ekaterina1971 <ivanova71dom@yandex.ru>
2024-11-14 09:28:07 +0300

    домашнее задание

--- src/main/java/org/skypro/list/EmployeeController.java
+++ src/main/java/org/skypro/list/EmployeeController.java
@@ -50,52 +50,6 @@ public class EmployeeController {
 
 
 
-//public class EmployeeController {
-  //  private final EmployeeService employeeService;
-    //public EmployeeController(EmployeeService employeeService){
-      //  this.employeeService = employeeService;
-   // }
-   // @GetMapping("/add")
-  //  public Employee addEmployee(@RequestParam("firstName") String firstName,
-       //                 @RequestParam("lastName") String lastName) {
-    //    return employeeService.add(firstName, lastName);
-  //  }
-        // employeeService.add(firstName, lastName);
-    // catch (EmployeeStoragelsFullException employeeStoragelsFullException){
-      // System.out.println("Превышен лимит сотрудников");
-    //catch (EmployeeAlreadyAddedException employeeAlreadyAddedException) {
-       // System.out.println("Такой сотрудник уже существует");
-
-    //Employee employee = new Employee(firstName, lastName);
-   // return employee;
-
-//@GetMapping("/remove")
-  //  public Employee removeEmployee(@RequestParam("firstName") String firstName,
-          //                 @RequestParam("lastName") String lastName) {
- //   return employeeService.remove(firstName, lastName);
- //   }
-    //try {
-      //  employeeService.remove(firstName, lastName);
-   // } catch (EmployeeNotFoundException employeeNotFoundException) {
-       // System.out.println("Сотрудник не найден");
-   // }   System.out.println("Сотрудник удален");
-   // return null;
-
-//@GetMapping("/find")
-  //  public Employee find(@RequestParam String firstName, @RequestParam String lastName) {
-      //  return service.find(firstName, lastName);
-  //  }
-//@GetMapping("/allEmployees")
-  //  public List<Employee> getAllEmployees() {
-     //   return employeeService.getAll();
-   // }
-
-       // try {
-           // employeeService.find(firstName, lastName);
-      //  } catch (EmployeeNotFoundException employeeNotFoundException) {
-          //  System.out.println("Сотрудник не найден");
-       // } return firstName + lastName;
-
 
 
 
